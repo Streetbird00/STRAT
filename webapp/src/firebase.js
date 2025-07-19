46e518f6bc8aa66f61285f5a51d20d179aaba97a
Streetbird00 <boxteljonathan@gmail.com>
2025-07-19 13:40:55 +0300

    update

--- webapp/src/firebase.js
+++ webapp/src/firebase.js
@@ -1,21 +1,32 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
+
   apiKey: "AIzaSyDSE1qFIKXYkfx7aK90YvrT0vvJYeJ6Uwk",
+
   authDomain: "stratcom-estate.firebaseapp.com",
+
   projectId: "stratcom-estate",
+
   storageBucket: "stratcom-estate.firebasestorage.app",
+
   messagingSenderId: "606318099680",
+
   appId: "1:606318099680:web:36446f8838b81f8e392485",
+
   measurementId: "G-SE3XYDRMJE"
+
 };
 
-// Initialize Firebase
+
+
+
+
+
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
