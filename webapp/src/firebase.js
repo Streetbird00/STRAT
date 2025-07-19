a55cc7cbc659b0ba5773d3c6e1896b8cc55309f5
Streetbird00 <boxteljonathan@gmail.com>
2025-07-19 13:34:16 +0300

    update

--- webapp/src/firebase.js
+++ webapp/src/firebase.js
@@ -1,16 +1,21 @@
-// firebase.js
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDyhlIhUZnvwv1Kwt45PJLOS5q9xu1qwuo",
-    authDomain: "realestate-edc8a.firebaseapp.com",
-    projectId: "realestate-edc8a",
-    storageBucket: "realestate-edc8a.firebasestorage.app",
-    messagingSenderId: "560090370426",
-    appId: "1:560090370426:web:959fb4571026bc0e4c06c3",
-    measurementId: "G-KF52PKG147"
-  };
+  apiKey: "AIzaSyDSE1qFIKXYkfx7aK90YvrT0vvJYeJ6Uwk",
+  authDomain: "stratcom-estate.firebaseapp.com",
+  projectId: "stratcom-estate",
+  storageBucket: "stratcom-estate.firebasestorage.app",
+  messagingSenderId: "606318099680",
+  appId: "1:606318099680:web:36446f8838b81f8e392485",
+  measurementId: "G-SE3XYDRMJE"
+};
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
